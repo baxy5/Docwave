0f7d1c1a3808766a7a31bb76a9e75a702879d1f9
baxy5 <bakos.soda@gmail.com>
2025-05-12 23:15:12 +0200

    dockerized nodejs server

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM node:20-alpine3.20
+
+WORKDIR /app
+
+# Copy only package files first for caching
+
+# It allows Docker to cache the npm install 
+# layer. If you only change your application 
+# code, Docker doesn't have to re-run npm 
+# install every time you rebuild the image, 
+# making builds much faster.
+COPY package*.json .env ./
+
+RUN npm install --production
+
+COPY . /app
+
+ENV HOSTNAME=0.0.0.0
+
+EXPOSE 6543
+
+CMD ["node" , "app.js"]
+
---
+++ app.js
@@ -0,0 +1,81 @@
+const http = require("node:http");
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
+
+// localhost is not good if I want to dockerize, because
+// it would only run in the docker container enviromnent
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const port = 6543;
+const dynamicData = {
+  title: "DocWave",
+  inputTitle: "url / File selector",
+  inputPlaceholder: "no url provided.",
+  outputTitle: "Summary",
+};
+
+async function handleRequest(req, res) {
+  try {
+    if (req.method === "GET" && req.url === "/") {
+      await serveHtml(req, res);
+    } else if (req.method === "GET" && req.url === "/dist/output.css") {
+      await serveCss(req, res);
+    } else {
+      await notFound(req, res);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function serveHtml(req, res) {
+  const filePath = path.join(__dirname + "/index.html");
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "text/plain");
+      res.end("Internal server error.");
+      return;
+    }
+
+    let modifiedData = data;
+    for (const [key, value] of Object.entries(dynamicData)) {
+      modifiedData = modifiedData.replace(`{{ ${key} }}`, value);
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(modifiedData);
+  });
+}
+
+async function serveCss(req, res) {
+  const filePath = path.join(__dirname + "/dist/output.css");
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "text/plain");
+      res.end(`Internal server error.`);
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/css",
+    });
+    res.end(data);
+  });
+}
+
+async function notFound(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain" });
+  res.end("Not Found");
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at: http://${hostname}:${port}/`);
+});
