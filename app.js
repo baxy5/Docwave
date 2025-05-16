b9b83a3c905ed0d79a9093fec8555307e04bb9ec
baxy5 <bakos.soda@gmail.com>
2025-05-16 21:23:43 +0200

    implement to serve js files and markdown handling added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require("node:http");
 const fs = require("fs");
 const path = require("path");
+const { marked } = require("marked");
 
 require("dotenv").config();
 
@@ -14,6 +15,8 @@ const dynamicData = {
   inputTitle: "url / File selector",
   inputPlaceholder: "no url provided.",
   outputTitle: "Summary",
+  outputContent:
+    "# Example Markdown\n\n## H2 example\n\nThis is a simple markdown file with exactly twenty words. It demonstrates how markdown formatting works here.",
 };
 
 async function handleRequest(req, res) {
@@ -22,6 +25,8 @@ async function handleRequest(req, res) {
       await serveHtml(req, res);
     } else if (req.method === "GET" && req.url.includes(".css")) {
       await serveCss(req, res);
+    } else if (req.method === "GET" && req.url.includes(".js")) {
+      await serveScripts(req, res);
     } else {
       await notFound(req, res);
     }
@@ -42,6 +47,9 @@ async function serveHtml(req, res) {
     }
 
     let modifiedData = data;
+
+    dynamicData.outputContent = marked.parse(dynamicData.outputContent);
+
     for (const [key, value] of Object.entries(dynamicData)) {
       modifiedData = modifiedData.replace(`{{ ${key} }}`, value);
     }
@@ -61,10 +69,29 @@ async function serveCss(req, res) {
       res.statusCode = 500;
       res.setHeader("Content-Type", "text/plain");
       res.end(`Internal server error.`);
+      return;
     }
 
     res.writeHead(200, {
       "Content-Type": "text/css",
+    });
+    res.end(data);
+  });
+}
+
+async function serveScripts(req, res) {
+  const filePath = path.join(__dirname + req.url);
+
+  fs.readFile(filePath, "utf-8", (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "text/plain");
+      res.end(`Internal server error.`);
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/javascript",
     });
     res.end(data);
   });
